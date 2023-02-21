434255e6ebe7a74bf99778cd9a34568dbcf143ea
JustinM099 <justinm099@mac.com>
2023-02-20 22:47:50 -0800

    fixing navbar bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ function Navbar() {
                         variant="h6"
                         noWrap
                         component="a"
-                        href="/Home"
+                        href="/"
                         sx={{
                             mr: 2,
                             display: { xs: 'none', md: 'flex' },
@@ -99,7 +99,7 @@ function Navbar() {
                         variant="h5"
                         noWrap
                         component="a"
-                        href="/Home"
+                        href="/"
                         sx={{
                             mr: 2,
                             display: { xs: 'flex', md: 'none' },
